277e53c9d66977ba15a33c1208db32ec9bfdd0a0
Praneshwar24 <praneshwar.prakasam24@gmail.com>
2021-11-15 15:45:44 +0530

    Initial commit

---
+++ src/main/kotlin/lamdas/MyDrawable.java
@@ -0,0 +1,11 @@
+package lamdas;
+
+/**
+ * Implementation of interface
+ */
+public class MyDrawable implements Drawable{
+    @Override
+    public void draw() {
+        System.out.println("I am drawing a picture");
+    }
+}
